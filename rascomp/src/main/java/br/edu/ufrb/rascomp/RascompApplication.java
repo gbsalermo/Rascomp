3a427581d7c0a68edbeb2ad6c32ca0e75545509a
Salermo <119875310+gbsalermo@users.noreply.github.com>
2026-08-04 21:07:42 -0300

    feat: adiciona classe principal do Spring Boot

---
+++ rascomp/src/main/java/br/edu/ufrb/rascomp/RascompApplication.java
@@ -0,0 +1,12 @@
+package br.edu.ufrb.rascomp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RascompApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RascompApplication.class, args);
+    }
+}
