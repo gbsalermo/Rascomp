c6b26aeb25a4727728f5c373ba1a967e0e6e2a78
Salermo <119875310+gbsalermo@users.noreply.github.com>
2026-08-06 20:42:10 -0300

    feat: adiciona repository de TentativaSeguidorLinha

---
+++ rascomp/src/main/java/br/edu/ufrb/rascomp/repository/TentativaSeguidorLinhaRepository.java
@@ -0,0 +1,15 @@
+package br.edu.ufrb.rascomp.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.edu.ufrb.rascomp.model.TentativaSeguidorLinha;
+
+@Repository
+public interface TentativaSeguidorLinhaRepository extends JpaRepository<TentativaSeguidorLinha, Long> {
+    List<TentativaSeguidorLinha> findByRegistrationIdOrderByTomadaAscNumeroTentativaAsc(Long registrationId);
+    boolean existsByRegistrationIdAndTomadaAndNumeroTentativa(Long registrationId, Integer tomada, Integer numeroTentativa);
+    boolean existsByRegistrationIdAndTomadaAndNumeroTentativaAndIdNot(Long registrationId, Integer tomada, Integer numeroTentativa, Long id);
+}
