00786ed9b8f95d0d7f6f6ca020f37295b9e83019
Salermo <119875310+gbsalermo@users.noreply.github.com>
2026-08-06 20:40:44 -0300

    feat: adiciona repository de Registration

---
+++ rascomp/src/main/java/br/edu/ufrb/rascomp/repository/RegistrationRepository.java
@@ -0,0 +1,19 @@
+package br.edu.ufrb.rascomp.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.edu.ufrb.rascomp.model.Registration;
+import br.edu.ufrb.rascomp.model.Enum.StatusRegistration;
+
+@Repository
+public interface RegistrationRepository extends JpaRepository<Registration, Long> {
+    List<Registration> findAllByOrderByDataCadastroDesc();
+    List<Registration> findByAtivoTrueOrderByDataCadastroDesc();
+    List<Registration> findByCompetitionIdOrderByDataCadastroDesc(Long competitionId);
+    List<Registration> findByStatusOrderByDataCadastroDesc(StatusRegistration status);
+    boolean existsByCompetitionIdAndCategoryIdAndRobotId(Long competitionId, Long categoryId, Long robotId);
+    boolean existsByCompetitionIdAndCategoryIdAndRobotIdAndIdNot(Long competitionId, Long categoryId, Long robotId, Long id);
+}
