6ec48f1ee2829d0ef46e2267777a7e40f62765a8
Salermo <119875310+gbsalermo@users.noreply.github.com>
2026-08-06 20:38:41 -0300

    feat: adiciona status de competição

---
+++ rascomp/src/main/java/br/edu/ufrb/rascomp/model/Enum/StatusCompetition.java
@@ -0,0 +1,10 @@
+package br.edu.ufrb.rascomp.model.Enum;
+
+public enum StatusCompetition {
+    PLANEJADA,
+    INSCRICOES_ABERTAS,
+    INSCRICOES_ENCERRADAS,
+    EM_ANDAMENTO,
+    FINALIZADA,
+    CANCELADA
+}
