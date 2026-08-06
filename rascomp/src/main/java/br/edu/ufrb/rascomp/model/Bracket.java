440a19acd89ed59a96f0d264b63f341912a371d1
Salermo <119875310+gbsalermo@users.noreply.github.com>
2026-08-06 20:42:51 -0300

    feat: adiciona entidade Bracket

---
+++ rascomp/src/main/java/br/edu/ufrb/rascomp/model/Bracket.java
@@ -0,0 +1,66 @@
+package br.edu.ufrb.rascomp.model;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import br.edu.ufrb.rascomp.model.Enum.StatusBracket;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(
+    name = "brackets",
+    uniqueConstraints = @UniqueConstraint(
+        name = "uk_bracket_competition_category",
+        columnNames = {"competition_id", "category_id"}
+    )
+)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Bracket implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "competition_id", nullable = false)
+    private Competition competition;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "category_id", nullable = false)
+    private CompetitionCategory category;
+
+    @Column(nullable = false, length = 150)
+    private String nome;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private StatusBracket status;
+
+    @Column(nullable = false)
+    private Boolean ativo = true;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime dataCadastro;
+}
