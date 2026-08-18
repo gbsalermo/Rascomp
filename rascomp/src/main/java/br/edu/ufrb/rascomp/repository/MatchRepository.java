e8b5cfaf54301858abe6777608973b34d207b001
Salermo <119875310+gbsalermo@users.noreply.github.com>
2026-08-17 23:21:18 -0300

    feat: adiciona busca de partida por posicao no chaveamento

--- rascomp/src/main/java/br/edu/ufrb/rascomp/repository/MatchRepository.java
+++ rascomp/src/main/java/br/edu/ufrb/rascomp/repository/MatchRepository.java
@@ -1,6 +1,7 @@
 package br.edu.ufrb.rascomp.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,6 +11,7 @@ import br.edu.ufrb.rascomp.model.Match;
 @Repository
 public interface MatchRepository extends JpaRepository<Match, Long> {
     List<Match> findByBracketIdOrderByRodadaAscOrdemAsc(Long bracketId);
+    Optional<Match> findByBracketIdAndRodadaAndOrdem(Long bracketId, Integer rodada, Integer ordem);
     boolean existsByBracketIdAndRodadaAndOrdem(Long bracketId, Integer rodada, Integer ordem);
     boolean existsByBracketIdAndRodadaAndOrdemAndIdNot(Long bracketId, Integer rodada, Integer ordem, Long id);
 }
