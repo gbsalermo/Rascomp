94a6bb7b57308f466c4afcf55014cf30bf3480c0
Salermo <119875310+gbsalermo@users.noreply.github.com>
2026-08-24 21:45:59 -0300

    documenta payload de login no OpenAPI

--- rascomp/src/main/java/br/edu/ufrb/rascomp/dto/LoginRequest.java
+++ rascomp/src/main/java/br/edu/ufrb/rascomp/dto/LoginRequest.java
@@ -1,5 +1,6 @@
 package br.edu.ufrb.rascomp.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
@@ -7,11 +8,14 @@ import lombok.Setter;
 
 @Getter
 @Setter
+@Schema(description = "Credenciais para autenticação no Rascomp.")
 public class LoginRequest {
     @NotBlank
     @Email
+    @Schema(example = "participante@exemplo.com")
     private String email;
 
     @NotBlank
+    @Schema(example = "SenhaForte@2026", format = "password", writeOnly = true)
     private String senha;
 }
