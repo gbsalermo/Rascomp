1872c03d9559115f77530e2a2694e11940cd3693
Salermo <119875310+gbsalermo@users.noreply.github.com>
2026-08-25 22:02:08 -0300

    list follow attempts by competition context

--- rascomp/src/main/java/br/edu/ufrb/rascomp/repository/TentativaSeguidorLinhaRepository.java
+++ rascomp/src/main/java/br/edu/ufrb/rascomp/repository/TentativaSeguidorLinhaRepository.java
@@ -10,6 +10,11 @@ import br.edu.ufrb.rascomp.model.TentativaSeguidorLinha;
 @Repository
 public interface TentativaSeguidorLinhaRepository extends JpaRepository<TentativaSeguidorLinha, Long> {
     List<TentativaSeguidorLinha> findByRegistrationIdOrderByTomadaAscNumeroTentativaAsc(Long registrationId);
+
+    List<TentativaSeguidorLinha> findByRegistrationCompetitionIdAndRegistrationCategoryIdOrderByDataCadastroDesc(
+            Long competitionId,
+            Long categoryId);
+
     boolean existsByRegistrationIdAndTomadaAndNumeroTentativa(Long registrationId, Integer tomada, Integer numeroTentativa);
     boolean existsByRegistrationIdAndTomadaAndNumeroTentativaAndIdNot(Long registrationId, Integer tomada, Integer numeroTentativa, Long id);
 }
