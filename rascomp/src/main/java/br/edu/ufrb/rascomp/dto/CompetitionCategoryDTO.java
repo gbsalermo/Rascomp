f3e0fda4f4b1b99501f423168eefcacde054c8a9
Salermo <119875310+gbsalermo@users.noreply.github.com>
2026-09-06 16:06:15 -0300

    feat: expor classe fisica nas categorias

--- rascomp/src/main/java/br/edu/ufrb/rascomp/dto/CompetitionCategoryDTO.java
+++ rascomp/src/main/java/br/edu/ufrb/rascomp/dto/CompetitionCategoryDTO.java
@@ -1,9 +1,8 @@
 package br.edu.ufrb.rascomp.dto;
 
-
-
 import br.edu.ufrb.rascomp.model.CompetitionCategory;
 import br.edu.ufrb.rascomp.model.Enum.Modalidade;
+import br.edu.ufrb.rascomp.model.Enum.SumoPhysicalClass;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -11,32 +10,32 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class CompetitionCategoryDTO {
-	
-	private Long id;
-	
-	@NotBlank(message = "Nome é obrigatório")
-	private String nome;
-	
-	private String descricao;
-	
-	@NotNull(message = "Modalidade é obrigatoria")
-	private Modalidade modalidade;
-	
-	private Boolean ativo;
-	
-	public CompetitionCategoryDTO( CompetitionCategory entity) {
-		
-		this.id = entity.getId();
-		this.nome = entity.getNome();
-		this.descricao = entity.getDescricao();
-		this.modalidade = entity.getModalidade();
-		this.ativo = entity.getAtivo();
-	}
 
+    private Long id;
+
+    @NotBlank(message = "Nome é obrigatório")
+    private String nome;
+
+    private String descricao;
+
+    @NotNull(message = "Modalidade é obrigatoria")
+    private Modalidade modalidade;
+
+    private SumoPhysicalClass sumoPhysicalClass;
+
+    private Boolean ativo;
+
+    public CompetitionCategoryDTO(CompetitionCategory entity) {
+        this.id = entity.getId();
+        this.nome = entity.getNome();
+        this.descricao = entity.getDescricao();
+        this.modalidade = entity.getModalidade();
+        this.sumoPhysicalClass = entity.getSumoPhysicalClass();
+        this.ativo = entity.getAtivo();
+    }
 }
