aa039d38c2a05b7000bb3346760af753447eaaf0
Salermo <119875310+gbsalermo@users.noreply.github.com>
2026-09-06 20:33:21 -0300

    feat: adicionar repositorio de ausencias Follow

---
+++ rascomp/src/main/java/br/edu/ufrb/rascomp/repository/AusenciaTomadaSeguidorLinhaRepository.java
@@ -0,0 +1,20 @@
+package br.edu.ufrb.rascomp.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.edu.ufrb.rascomp.model.AusenciaTomadaSeguidorLinha;
+
+@Repository
+public interface AusenciaTomadaSeguidorLinhaRepository extends JpaRepository<AusenciaTomadaSeguidorLinha, Long> {
+
+    List<AusenciaTomadaSeguidorLinha> findByRegistrationIdOrderByTomadaAsc(Long registrationId);
+
+    List<AusenciaTomadaSeguidorLinha> findByRegistrationCompetitionIdAndRegistrationCategoryIdOrderByDataCadastroDesc(
+            Long competitionId,
+            Long categoryId);
+
+    boolean existsByRegistrationIdAndTomada(Long registrationId, Integer tomada);
+}
