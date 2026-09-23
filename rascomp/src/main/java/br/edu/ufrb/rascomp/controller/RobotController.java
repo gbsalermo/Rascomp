09d75b3d893efefb2d242985a058057dd092fd09
Salermo <119875310+gbsalermo@users.noreply.github.com>
2026-09-23 00:03:15 -0300

    security: limitar catalogos globais a dev

--- rascomp/src/main/java/br/edu/ufrb/rascomp/controller/RobotController.java
+++ rascomp/src/main/java/br/edu/ufrb/rascomp/controller/RobotController.java
@@ -39,6 +39,7 @@ public class RobotController {
     }
 
     @GetMapping
+    @PreAuthorize("hasRole('DEV')")
     public ResponseEntity<List<RobotDTO>> listar(
             @RequestParam(defaultValue = "false") boolean apenasAtivos) {
 
@@ -50,6 +51,7 @@ public class RobotController {
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole('DEV')")
     public ResponseEntity<RobotDTO> buscarPorId(
             @PathVariable Long id) {
 
@@ -59,6 +61,7 @@ public class RobotController {
     }
 
     @GetMapping("/por-equipe")
+    @PreAuthorize("hasRole('DEV')")
     public ResponseEntity<List<RobotDTO>> listarPorEquipe(
             @RequestParam Long teamId,
             @RequestParam(defaultValue = "false") boolean apenasAtivos) {
