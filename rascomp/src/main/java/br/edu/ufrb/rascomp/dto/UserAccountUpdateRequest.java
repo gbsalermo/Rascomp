af17636d9c6d08f9f7d6e6ff82286fd9c3e39a1a
Salermo <119875310+gbsalermo@users.noreply.github.com>
2026-09-22 23:51:25 -0300

    feat: adicionar contrato de edicao cadastral de usuario

---
+++ rascomp/src/main/java/br/edu/ufrb/rascomp/dto/UserAccountUpdateRequest.java
@@ -0,0 +1,24 @@
+package br.edu.ufrb.rascomp.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserAccountUpdateRequest {
+
+    @NotBlank
+    @Size(max = 150)
+    private String nome;
+
+    @NotBlank
+    @Email
+    @Size(max = 150)
+    private String email;
+
+    @Size(max = 20)
+    private String telefone;
+}
