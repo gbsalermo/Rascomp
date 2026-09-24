30e855425bbe553f0bc780708435d4bb015e4c84
Salermo <119875310+gbsalermo@users.noreply.github.com>
2026-09-23 23:18:19 -0300

    feat: tipar fila de convocacao follow

---
+++ rascomp/src/main/java/br/edu/ufrb/rascomp/model/Enum/StatusConvocacaoFollow.java
@@ -0,0 +1,10 @@
+package br.edu.ufrb.rascomp.model.Enum;
+
+public enum StatusConvocacaoFollow {
+    AGUARDANDO,
+    CONVOCADA,
+    EM_APRESENTACAO,
+    EM_EXECUCAO,
+    CONCLUIDA,
+    AUSENTE
+}
