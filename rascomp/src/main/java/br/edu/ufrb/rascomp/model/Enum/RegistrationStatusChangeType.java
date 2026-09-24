48d7909940083f23ab571b7f277bd07072fd6a70
Salermo <119875310+gbsalermo@users.noreply.github.com>
2026-09-23 22:13:57 -0300

    feat: tipar eventos de auditoria da inscricao

---
+++ rascomp/src/main/java/br/edu/ufrb/rascomp/model/Enum/RegistrationStatusChangeType.java
@@ -0,0 +1,11 @@
+package br.edu.ufrb.rascomp.model.Enum;
+
+public enum RegistrationStatusChangeType {
+    CRIACAO,
+    APROVACAO,
+    REJEICAO,
+    CANCELAMENTO,
+    DESISTENCIA,
+    REATIVACAO,
+    DESCLASSIFICACAO
+}
