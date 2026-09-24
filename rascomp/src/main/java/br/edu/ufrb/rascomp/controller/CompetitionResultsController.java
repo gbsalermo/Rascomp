4b45c835c5f945de989cac4b27304e5b55ddfe6c
Salermo <119875310+gbsalermo@users.noreply.github.com>
2026-09-24 09:21:51 -0300

    feat: expor decisao administrativa do resultado follow

--- rascomp/src/main/java/br/edu/ufrb/rascomp/controller/CompetitionResultsController.java
+++ rascomp/src/main/java/br/edu/ufrb/rascomp/controller/CompetitionResultsController.java
@@ -4,12 +4,18 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.ufrb.rascomp.dto.CompetitionCategoryResultDTO;
+import br.edu.ufrb.rascomp.dto.FollowManualResultDTO;
+import br.edu.ufrb.rascomp.dto.FollowManualResultRequest;
 import br.edu.ufrb.rascomp.service.CompetitionResultsService;
+import br.edu.ufrb.rascomp.service.FollowManualResultService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -18,10 +24,17 @@ import lombok.RequiredArgsConstructor;
 public class CompetitionResultsController {
 
     private final CompetitionResultsService service;
+    private final FollowManualResultService followManualResultService;
 
     @GetMapping
     public ResponseEntity<List<CompetitionCategoryResultDTO>> listar(
             @RequestParam Long competitionId) {
         return ResponseEntity.ok(service.listar(competitionId));
+    }
+
+    @PostMapping("/follow/decisao-organizacao")
+    public ResponseEntity<FollowManualResultDTO> definirFollowPorOrganizacao(
+            @Valid @RequestBody FollowManualResultRequest request) {
+        return ResponseEntity.ok(followManualResultService.definir(request));
     }
 }
