5d2384ce36734924d2898494e4d999951d634e61
Salermo <119875310+gbsalermo@users.noreply.github.com>
2026-09-23 23:08:33 -0300

    feat: expor resolucao administrativa de partida

--- rascomp/src/main/java/br/edu/ufrb/rascomp/controller/MatchResultController.java
+++ rascomp/src/main/java/br/edu/ufrb/rascomp/controller/MatchResultController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +26,12 @@ public class MatchResultController {
     private final BracketService bracketService;
     private final MatchService matchService;
     private final CompetitionContextService competitionContextService;
+
+    @PostMapping("/resolver-indisponibilidade")
+    public ResponseEntity<MatchResultDTO> resolverIndisponibilidade(
+            @RequestParam Long matchId) {
+        return ResponseEntity.ok(resultService.resolverIndisponibilidadeAdministrativa(matchId));
+    }
 
     @GetMapping
     @PreAuthorize("hasRole('DEV')")
